34a995ffdd37f1f33aac2188e968e4ffe8852f77
Jimmy Guzman <jimmy.guzman@outlook.com>
2018-03-21 11:16:02 -0500

    limit search field to only search route

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SearchForm from "./SearchForm";
 import Home from "./Home";
 import Nav from "./Nav";
+import Results from "./Results";
 
 const App = () => (
   <BrowserRouter>
@@ -10,8 +11,8 @@ const App = () => (
       <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/search" component={SearchForm} />
-
+        <Route exact path="/search/" component={SearchForm} />
+        <Route exact path="/search/:query" component={Results} />
         <Route
           render={() => (
             <h2>404 error: URL does not match any existing paths!</h2>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,26 @@
 import React, { Component, Fragment } from "react";
-import { NavLink } from "react-router-dom";
-import { Route } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import Results from "./Results";
 
 class Nav extends Component {
   render() {
     return (
-      <Fragment>
-        <nav className="main-nav">
-          <ul>
-            <li>
-              <NavLink to="/search/">Search</NavLink>
-            </li>
-            <li>
-              <NavLink to="/search/penguins">Penguins</NavLink>
-            </li>
-            <li>
-              <NavLink to="/search/skateboarding">Skateboarding</NavLink>
-            </li>
-            <li>
-              <NavLink to="/search/axolotl">Axolotl</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <Route exact path="/search/:query" component={Results} />
-      </Fragment>
+      <nav className="main-nav">
+        <ul>
+          <li>
+            <Link to="/search/">Search</Link>
+          </li>
+          <li>
+            <NavLink to="/search/penguins">Penguins</NavLink>
+          </li>
+          <li>
+            <NavLink to="/search/skateboarding">Skateboarding</NavLink>
+          </li>
+          <li>
+            <NavLink to="/search/axolotl">Axolotl</NavLink>
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
