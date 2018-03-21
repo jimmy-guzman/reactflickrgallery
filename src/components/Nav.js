0fb5bd8c0e3317171c903265ba6648ad9a282a2f
Jimmy Guzman <jimmy.guzman@outlook.com>
2018-03-20 19:28:11 -0500

    fixed link width, and removed extra divs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,21 +4,19 @@ import { NavLink } from "react-router-dom";
 class Nav extends Component {
   render() {
     return (
-      <div>
-        <nav className="main-nav">
-          <ul>
-            <li>
-              <NavLink to="/search/penguins">Penguins</NavLink>
-            </li>
-            <li>
-              <NavLink to="/search/skateboarding">Skateboarding</NavLink>
-            </li>
-            <li>
-              <NavLink to="/search/axolotl">Axolotl</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <nav className="main-nav">
+        <ul>
+          <li>
+            <NavLink to="/search/penguins">Penguins</NavLink>
+          </li>
+          <li>
+            <NavLink to="/search/skateboarding">Skateboarding</NavLink>
+          </li>
+          <li>
+            <NavLink to="/search/axolotl">Axolotl</NavLink>
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
